fdb21064eff16dcf3b113e9675bfe05a6a71aaca
Harshita Sagar <harshitasagar555@gmail.com>
2025-02-18 14:16:13 +0530

    Added Day1 - Brand Name Generator

---
+++ Day1_BrandNameGenerator.py
@@ -0,0 +1,5 @@
+print("Welcome to brand name generator")
+city_name = input("What is the name of the city you grew up?\n")
+pet_name = input("What is the name of your per?\n")
+print("Your brand name will be " + city_name + " " + pet_name)
+
