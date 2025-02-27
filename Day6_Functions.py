495776c75f7fca9b19468c4d1577463b09b6f346
Harshita Sagar <harshitasagar555@gmail.com>
2025-02-27 20:45:01 +0530

    Added Day6 Functions & Reeborg's World

---
+++ Day6_Functions.py
@@ -0,0 +1,23 @@
+def my_function():
+    print("Hello")
+    print("Bye")
+
+
+my_function()
+
+## REEBORG'S WORLD GAME
+## https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+#
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
